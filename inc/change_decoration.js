9d97c6f6b3fdf540dc3caa03d7c0765abafb989a
Addama <addama.sanders@gmail.com>
2021-12-29 15:41:57 -0800

    Even more simplified js

--- inc/change_decoration.js
+++ inc/change_decoration.js
@@ -1,5 +1,2 @@
-var isMobile = /iPhone|iPad|iPod|Android|BlackBerry|webOS|Windows Phone/i.test(navigator.userAgent);
-
-if (isMobile) {
-	document.getElementById('decoration').innerHTML = 'HI'
-}
+var isMobile = /iPhone|iPad|iPod|Android|BlackBerry|webOS|Windows Phone/i.test(navigator.userAgent)
+if (isMobile) document.getElementById('decoration').innerHTML = '&hearts;'
