e306a311e3b285297ca830adb7cc0950bd1818cc
Addama <addama.sanders@gmail.com>
2022-01-04 13:49:33 -0800

    Added comment

--- inc/change_decoration.js
+++ inc/change_decoration.js
@@ -1,2 +1,3 @@
+// Change fleuron UNICODE symbol to the much more widely implemented hearts symbol on mobile
 var isMobile = /iPhone|iPad|iPod|Android|BlackBerry|webOS|Windows Phone/i.test(navigator.userAgent)
 if (isMobile) document.getElementById('decoration').innerHTML = '&hearts;'
